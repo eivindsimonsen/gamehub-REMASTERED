c3903322c57497429b7511745788bfe8568a2538
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-04-16 17:42:38 +0200

    Basic react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 // styles
 import "./sass/style.scss";
+// pagages
+import { Routes, Route } from "react-router-dom";
 // pages
 import HeaderNav from "./components/nav/HeaderNav";
 import FooterNav from "./components/nav/FooterNav";
+import Home from "./components/pages/index/Home";
+import Games from "./components/pages/games/Games";
+import Sell from "./components/pages/sell/Sell";
+import Login from "./components/pages/login/Login";
 
 function App() {
   return (
     <>
       <HeaderNav />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/games"
+          element={<Games />}
+        />
+        <Route
+          path="/sell"
+          element={<Sell />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+      </Routes>
       <FooterNav />
     </>
   );
--- src/components/nav/HeaderNav.jsx
+++ src/components/nav/HeaderNav.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function HeaderNav() {
-  return <div>HeaderNav</div>;
+  return (
+    <nav>
+      <div className="brand-logo">
+        <NavLink to="/">LOGO</NavLink>
+      </div>
+      <ul>
+        <li>
+          <NavLink to="/games">Games</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sell">Sell game</NavLink>
+        </li>
+        <input type="text" />
+        <li>
+          <NavLink to="/login">Log in</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default HeaderNav;
