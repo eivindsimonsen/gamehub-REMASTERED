4ff3179b4afa436edd12aeaa1be00ac16daf6d3b
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-04-21 02:26:40 +0200

    Working out the header in index

--- src/components/pages/index/Home.jsx
+++ src/components/pages/index/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import svg from "../../../assets/undraw_virtual_reality_re_yg8i.svg";
+import svg from "../../../assets/undraw_gaming_re_cma2.svg";
 
 function Home() {
   return (
     <>
       <section className="home-intro">
-        <h1>The universe of games</h1>
         <img
           src={svg}
           alt="Man fitted into his chair with a VR headset attached to his head."
         />
+        <h1>
+          <span>The</span> <br />
+          <span>universe</span> <br />
+          <span>of</span> <br />
+          <span>games</span>
+        </h1>
       </section>
     </>
   );
