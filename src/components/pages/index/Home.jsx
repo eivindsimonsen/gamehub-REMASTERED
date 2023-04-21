4482c60325b181017c47ef3cf1fe3aab981efeba
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-04-21 19:13:42 +0200

    Popular cards added

--- src/components/pages/index/Home.jsx
+++ src/components/pages/index/Home.jsx
@@ -48,6 +48,47 @@ function Home() {
           </h1>
         </div>
       </section>
+      <section className="popular container">
+        <h2>Popular games</h2>
+        <hr />
+        <div className="popular-card-container">
+          <div className="popular-card">
+            <img
+              src={product1}
+              alt=""
+            />
+            <div>
+              <h3>Ping pong</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel consequuntur dolorem nemo eius saepe debitis, at nihil repudiandae, suscipit animi sunt? Cum dolores soluta provident ex, nisi temporibus id voluptate numquam perspiciatis, explicabo illo exercitationem architecto fuga, eum aliquid est. Cum dolores soluta provident ex, nisi temporibus id voluptate numquam perspiciatis, explicabo illo exercitationem architecto fuga, eum aliquid est.</p>
+              <button className="cta cta-primary">Read more</button>
+            </div>
+          </div>
+          <div className="popular-card">
+            <img
+              src={product2}
+              alt=""
+            />
+            <div>
+              <h3>Super duper</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel consequuntur dolorem nemo eius saepe debitis, at nihil repudiandae, suscipit animi sunt? Cum dolores soluta provident ex, nisi temporibus id voluptate numquam perspiciatis, explicabo illo exercitationem architecto fuga, eum aliquid est.</p>
+              <button className="cta cta-primary">Read more</button>
+            </div>
+          </div>
+          <div className="popular-card">
+            <img
+              src={product3}
+              alt=""
+            />
+            <div>
+              <h3>Black</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel consequuntur dolorem nemo eius saepe debitis, at nihil repudiandae, suscipit animi sunt? Cum dolores soluta provident ex, nisi temporibus id voluptate numquam perspiciatis, explicabo illo exercitationem architecto fuga, eum aliquid est.</p>
+              <button className="cta cta-primary">Read more</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="deals"></section>
+      <section className="reviews"></section>
     </>
   );
 }
