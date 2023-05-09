8ab005e8ddb6c27aec6f4797e900fec0b2730e26
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-10 00:07:32 +0200

    Begun login stylings

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
 function Login() {
-  return <div>Login</div>;
+  return (
+    <section id="login">
+      <div className="left-side">Login or create a user</div>
+      <form className="right-side login-form">
+        <input
+          type="text"
+          placeholder="Username/email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+        />
+        <button className="cta cta-primary">Login</button>
+        <button className="cta cta-alt">Create a user</button>
+      </form>
+    </section>
+  );
 }
 
 export default Login;
