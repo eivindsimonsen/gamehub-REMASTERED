1439e5ee81559de37f1f54cddf744e436b768f9c
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-11 20:30:24 +0200

    Login page complete + responsive

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,20 +1,30 @@
+import { NavLink } from "react-router-dom";
+
 function Login() {
   return (
-    <section id="login">
-      <div className="left-side">Login or create a user</div>
-      <form className="right-side login-form">
-        <input
-          type="text"
-          placeholder="Username/email"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-        />
-        <button className="cta cta-primary">Login</button>
-        <button className="cta cta-alt">Create a user</button>
-      </form>
-    </section>
+    <>
+      <NavLink
+        to="/"
+        className="return-btn">
+        <i className="fa-solid fa-arrow-left"></i>
+        <div>Return</div>
+      </NavLink>
+      <section id="login">
+        <h1 className="left-side">Login with Google, or create a new account</h1>
+        <form className="right-side login-form">
+          <input
+            type="text"
+            placeholder="Username/email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+          />
+          <button className="cta cta-primary">Login</button>
+          <button className="cta cta-alt">Create a user</button>
+        </form>
+      </section>
+    </>
   );
 }
 
