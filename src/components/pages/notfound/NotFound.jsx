787c343188dbb823020d0fe0d77ce9c7e350e68c
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-11 20:35:05 +0200

    Basic not-found page created

--- src/components/pages/notfound/NotFound.jsx
+++ src/components/pages/notfound/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return <h1 className="not-found-msg">Oh... Seems like this page is non-existant</h1>;
 }
 
 export default NotFound;
