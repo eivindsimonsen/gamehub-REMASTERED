4ac8a485e27d8296a820e934c639a299cf624582
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-12 21:54:20 +0200

    Component displays different content based on user state

--- src/components/pages/sell/Sell.jsx
+++ src/components/pages/sell/Sell.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 function Sell() {
-  return <div>Sell</div>;
+  const [user] = useState(false);
+
+  return (
+    <section id="sell">
+      {user ? (
+        <div>
+          <h1>You must log in to see your inventory of games</h1>
+        </div>
+      ) : (
+        <div className="sell-logged-out">
+          <h1>
+            You must <span className="purple-span">log in</span> to see your inventory of games
+          </h1>
+          <NavLink
+            to="/login"
+            className="cta cta-alt">
+            Go to login
+          </NavLink>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Sell;
