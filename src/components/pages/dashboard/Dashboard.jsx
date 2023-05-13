c93eeb2dc5e00b1578d66db8f662c93bb6b37fee
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-13 21:59:28 +0200

    Testing out layouts

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Hello user</div>;
+  return (
+    <section id="dashboard">
+      <aside>
+        <ul>
+          <li>Your account</li>
+          <li>Listed games</li>
+          <li>App settings</li>
+        </ul>
+      </aside>
+      <section className="dashboard-container">
+        <section>Your account</section>
+        <section>Listed games</section>
+        <section>App settings</section>
+      </section>
+    </section>
+  );
 }
 
 export default Dashboard;
