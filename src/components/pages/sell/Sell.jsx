4549746aa9b072b03f75d90b11a97eac3837b75e
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-13 21:32:09 +0200

    Simple design for sell page added

--- src/components/pages/sell/Sell.jsx
+++ src/components/pages/sell/Sell.jsx
@@ -1,25 +1,22 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import Login from "./../login/Login";
+import SellCard from "./SellCard";
 
 function Sell() {
-  const [user] = useState(false);
+  const [user] = useState(true);
 
   return (
     <section id="sell">
       {user ? (
-        <div>
-          <h1>You must log in to see your inventory of games</h1>
+        <div className="container">
+          <h1>Games you own: </h1>
+          <div>
+            <SellCard />
+          </div>
         </div>
       ) : (
-        <div className="sell-logged-out">
-          <h1>
-            You must <span className="purple-span">log in</span> to see your inventory of games
-          </h1>
-          <NavLink
-            to="/login"
-            className="cta cta-alt">
-            Go to login
-          </NavLink>
+        <div>
+          <Login />
         </div>
       )}
     </section>
