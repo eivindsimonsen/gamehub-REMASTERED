547fea48bc5e03fa10f9b56b787e89235abdc861
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-15 19:54:15 +0200

    Will now only show component which matches current index set on click

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+// hooks
+import { useState } from "react";
+// components
+import YourAccount from "./YourAccount";
+import ListedGames from "./ListedGames";
+import AppSettings from "./AppSettings";
 
 function Dashboard() {
+  const [selectedDashboard, setSelectedDashboard] = useState(0);
+
   return (
     <section id="dashboard">
       <aside>
         <ul>
-          <li>
-            <i className="fa-solid fa-user"></i> <span>Your account</span>
+          <li
+            id={selectedDashboard === 0 && "dashboard-active"}
+            onClick={() => setSelectedDashboard(0)}>
+            <i className="fa-solid fa-user"></i>
+            <span>Your account</span>
           </li>
-          <li>
-            <i className="fa-solid fa-list"></i> <span>Listed games</span>
+          <li
+            id={selectedDashboard === 1 && "dashboard-active"}
+            onClick={() => setSelectedDashboard(1)}>
+            <i className="fa-solid fa-list"></i>
+            <span>Listed games</span>
           </li>
-          <li>
-            <i class="fa-solid fa-gear"></i> <span>App settings</span>
+          <li
+            id={selectedDashboard === 2 && "dashboard-active"}
+            onClick={() => setSelectedDashboard(2)}>
+            <i class="fa-solid fa-gear"></i>
+            <span>App settings</span>
           </li>
         </ul>
       </aside>
       <section className="dashboard-container">
-        <div>Your account</div>
-        <div>Listed games</div>
-        <div>App settings</div>
+        <div>
+          {selectedDashboard === 0 && <YourAccount />}
+          {selectedDashboard === 1 && <ListedGames />}
+          {selectedDashboard === 2 && <AppSettings />}
+        </div>
       </section>
     </section>
   );
