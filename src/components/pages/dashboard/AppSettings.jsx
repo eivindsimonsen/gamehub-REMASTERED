16c038c67923ef1129656d834ba6ec68b06f6bcc
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-16 21:54:14 +0200

    Basic form added

--- src/components/pages/dashboard/AppSettings.jsx
+++ src/components/pages/dashboard/AppSettings.jsx
@@ -5,6 +5,12 @@ function AppSettings() {
     <>
       <section id="settings">
         <h1>App settings</h1>
+        <form>
+          <input type="text" />
+          <input type="radio" />
+          <input type="checkbox" />
+          <button>Update info</button>
+        </form>
       </section>
     </>
   );
