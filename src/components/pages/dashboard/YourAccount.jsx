6a01546986523c4cb6724bb33be0479cba70dfb9
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-05-18 15:13:02 +0200

    Account info layout added

--- src/components/pages/dashboard/YourAccount.jsx
+++ src/components/pages/dashboard/YourAccount.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+// images
+import image from "../../../assets/profile-image.jpg";
 
 function YourAccount() {
   return (
     <>
       <section id="your-account">
         <h1>Your account</h1>
+        <section className="your-account-content container">
+          <figure>
+            <img
+              src={image}
+              alt="Profile id"
+            />
+            <span>change</span>
+          </figure>
+          <div>
+            <p>
+              user id: <span>ørfljkha590234klhnsefhlksefj</span>
+            </p>
+            <i className="fa-solid fa-pen"></i>
+          </div>
+          <div>
+            <p>
+              username: <span>eivindsimonsen</span>
+            </p>
+            <i className="fa-solid fa-pen"></i>
+          </div>
+          <div>
+            <p>
+              email: <span>eivindsimonsen@gmail.com</span>
+            </p>
+            <i className="fa-solid fa-pen"></i>
+          </div>
+          <div>
+            <p>
+              password: <span className="your-account-content--password">ørfljkha590234klhnsefhlksefj</span>
+            </p>
+            <i className="fa-solid fa-pen"></i>
+          </div>
+        </section>
       </section>
     </>
   );
