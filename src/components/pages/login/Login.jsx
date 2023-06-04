b1b7822cbfde82010d12ccdaf996d3e6ee1cf422
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-06-04 13:19:23 +0200

    Simplified the code with components

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import CreateUser from "./CreateUser";
+import SignIn from "./SignIn";
 
 function Login() {
   const [createUser, setCreateUser] = useState(false);
 
   const toggleForm = () => {
     setCreateUser(!createUser);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted");
   };
 
   return (
@@ -24,43 +21,7 @@ function Login() {
       <section id="login">
         <h1 className="left-side">Login with Google, or create a new account</h1>
         <div className="login-form-container">
-          {createUser ? (
-            <form className="right-side login-form">
-              <input
-                type="text"
-                placeholder="Username"
-              />
-              <input
-                type="text"
-                placeholder="Username/email"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-              />
-              <button
-                type="submit"
-                className="cta cta-primary">
-                Create user
-              </button>
-            </form>
-          ) : (
-            <form className="right-side login-form">
-              <input
-                type="text"
-                placeholder="Username/email"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-              />
-              <button
-                type="submit"
-                className="cta cta-primary">
-                Login
-              </button>
-            </form>
-          )}
+          {createUser ? <CreateUser /> : <SignIn />}
           {createUser ? (
             <button
               type="button"
