88220e06c51e2b3a70b98d63884fd85980402c70
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-06-13 15:12:39 +0200

    Create user functionality added

--- src/components/pages/login/CreateUser.jsx
+++ src/components/pages/login/CreateUser.jsx
@@ -1,33 +1,55 @@
 import { useState } from "react";
+import { UserAuth } from "../../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
 
   const toggleEye = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await createUser(email, password);
+      navigate("/dashboard");
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+
   return (
     <>
-      <form className="right-side login-form">
+      <form
+        onSubmit={handleSubmit}
+        className="right-side login-form">
         <input
           type="text"
           placeholder="Username"
           value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="email"
           placeholder="Username/email"
           value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Password"
-            /* value={password} */
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <i
             onClick={toggleEye}
@@ -39,6 +61,7 @@ function CreateUser() {
           className="cta cta-primary">
           Create user
         </button>
+        {error}
       </form>
     </>
   );
