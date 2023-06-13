d321981170cc785d0f1b44b7679a7fa7e09c492a
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-06-13 15:12:49 +0200

    Only display content if logged in

--- src/components/pages/dashboard/YourAccount.jsx
+++ src/components/pages/dashboard/YourAccount.jsx
@@ -1,44 +1,56 @@
 // images
 import image from "../../../assets/profile-image.jpg";
 
-function YourAccount() {
+function YourAccount(props) {
+  const { user } = props;
+  console.log(user);
+
   return (
     <>
       <section id="your-account">
         <h1>Your account</h1>
-        <section className="your-account-content container">
-          <figure>
-            <img
-              src={image}
-              alt="Profile id"
-            />
-            <span>change</span>
-          </figure>
-          <div>
-            <p>
-              user id: <span>ørfljkha590234klhnsefhlksefj</span>
-            </p>
-            <i className="fa-solid fa-pen"></i>
-          </div>
-          <div>
-            <p>
-              username: <span>eivindsimonsen</span>
-            </p>
-            <i className="fa-solid fa-pen"></i>
-          </div>
-          <div>
-            <p>
-              email: <span>eivindsimonsen@gmail.com</span>
-            </p>
-            <i className="fa-solid fa-pen"></i>
-          </div>
-          <div>
-            <p>
-              password: <span className="your-account-content--password">ørfljkha590234klhnsefhlksefj</span>
-            </p>
-            <i className="fa-solid fa-pen"></i>
-          </div>
-        </section>
+        {user && (
+          <section className="your-account-content container">
+            <figure>
+              {!user.photoUrl ? (
+                <img
+                  src={image}
+                  alt="Profile id"
+                />
+              ) : (
+                <img
+                  src={user.photoUrl}
+                  alt="Profile id"
+                />
+              )}
+              <span>change</span>
+            </figure>
+            <div>
+              <p>
+                user id: <span>{user.uid}</span>
+              </p>
+              <i className="fa-solid fa-pen"></i>
+            </div>
+            <div>
+              <p>
+                username: <span>{user.displayName}</span>
+              </p>
+              <i className="fa-solid fa-pen"></i>
+            </div>
+            <div>
+              <p>
+                email: <span>{user.email}</span>
+              </p>
+              <i className="fa-solid fa-pen"></i>
+            </div>
+            <div>
+              <p>
+                password: <span className="your-account-content--password">ørfljkha590234klhnsefhlksefj</span>
+              </p>
+              <i className="fa-solid fa-pen"></i>
+            </div>
+          </section>
+        )}
       </section>
     </>
   );
