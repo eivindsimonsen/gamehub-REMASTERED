6087405a7ef6eec460b9d794c4bff7b23cf0b858
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-06-20 15:42:31 +0200

    Setting up review posting

--- src/components/pages/index/PostReview.jsx
+++ src/components/pages/index/PostReview.jsx
@@ -1,37 +1,52 @@
 import { useState } from "react";
+import { UserAuth } from "../../../context/AuthContext";
+import { Link } from "react-router-dom";
 
 function PostReview() {
+  const { user } = UserAuth();
   const [rating, setRating] = useState(0);
+
+  console.log(user);
 
   const chooseRating = (value) => {
     setRating(value);
   };
 
   return (
-    <form className="home-form">
-      <textarea
-        name="message"
-        id=""
-        rows="4"
-        placeholder="What do you think of this website?"></textarea>
-      <div>
-        {[1, 2, 3, 4, 5].map((value) => (
-          <label
-            className="rating-review"
-            key={value}>
-            <input
-              type="radio"
-              value={value}
-              name="stars"
-              checked={rating === value}
-              onChange={() => chooseRating(value)}
-            />
-            <i className={"fa-star " + (rating >= value ? "fa-solid" : "fa-regular")}></i>
-          </label>
-        ))}
-      </div>
-      <button className="cta cta-alt">Post</button>
-    </form>
+    <>
+      {user ? (
+        <form className="home-form">
+          <textarea
+            name="message"
+            id=""
+            rows="4"
+            placeholder="What do you think of this website?"></textarea>
+          <div>
+            {[1, 2, 3, 4, 5].map((value) => (
+              <label
+                className="rating-review"
+                key={value}>
+                <input
+                  type="radio"
+                  value={value}
+                  name="stars"
+                  checked={rating === value}
+                  onChange={() => chooseRating(value)}
+                />
+                <i className={"fa-star " + (rating >= value ? "fa-solid" : "fa-regular")}></i>
+              </label>
+            ))}
+          </div>
+          <button className="cta cta-alt">Post</button>
+        </form>
+      ) : (
+        <Link
+          to="/login"
+          className="cta cta-alt">
+          Log in to comment..
+        </Link>
+      )}
+    </>
   );
 }
 
