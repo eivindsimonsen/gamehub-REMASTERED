19aeaae4cd32dc86546c685ea803f8dba856c032
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-06-20 13:57:29 +0200

    Removed cons log

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -20,7 +20,7 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser);
+      // console.log(currentUser);
       setUser(currentUser);
     });
     return () => {
