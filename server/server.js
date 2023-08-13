bbf7f71b08eb044eaca09a717af90e9401349406
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-08-13 22:38:44 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 // server/server.js
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../.env") });
+require("dotenv").config({ path: path.join(__dirname, "../env") });
 
 const express = require("express");
 const app = express();
